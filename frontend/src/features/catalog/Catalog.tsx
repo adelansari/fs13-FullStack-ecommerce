00c7a7114807e6937acb969283d8015fdb0c28eb
Adel Ansari <adelansari@github.com>
2023-04-20 09:11:48 +0300

    delay and loading indicator

--- frontend/src/features/catalog/Catalog.tsx
+++ frontend/src/features/catalog/Catalog.tsx
@@ -1,18 +1,21 @@
 import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
 
 export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         agent.Catalog.list()
             .then(products => {
                 setProducts(products)
             })
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
     }, [])
-
     // for testing
     // function addProduct() {
     //     setProducts(prevState => [...prevState, {
@@ -24,6 +27,8 @@ export default function Catalog() {
     //         pictureUrl: 'http://picsum.photos/200',
     //     }])
     // }
+
+    if (loading) return <LoadingComponent message="Loading products..." />
     return (
         <>
             <ProductList products={products} />
--- frontend/src/features/catalog/ProductDetails.tsx
+++ frontend/src/features/catalog/ProductDetails.tsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFound";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 const ProductImage = styled('img')({
     maxWidth: '100%',
@@ -39,11 +40,11 @@ export default function ProductDetails() {
     useEffect(() => {
         id && agent.Catalog.details(parseInt(id))
             .then(response => setProduct(response))
-            .catch(error => console.log(error.response))
+            .catch(error => console.log(error))
             .finally(() => setLoading(false))
     }, [id]);
 
-    if (loading) return <h3>Loading...</h3>
+    if (loading) return <LoadingComponent message="Loading product..." />
 
     if (!product) return <NotFound />
 
