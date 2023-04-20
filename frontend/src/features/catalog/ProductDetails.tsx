3948b91591d5edc049c0eb415825445f344d5be8
Adel Ansari <adelansari@github.com>
2023-04-20 11:50:30 +0300

    product detail has a basket feature now

--- frontend/src/features/catalog/ProductDetails.tsx
+++ frontend/src/features/catalog/ProductDetails.tsx
@@ -1,19 +1,21 @@
-import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography, styled } from "@mui/material";
+import { Box, Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography, styled } from "@mui/material";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFound";
 import LoadingComponent from "../../app/layout/LoadingComponent";
+import { useStoreContext } from "../../app/context/StoreContext";
+import { LoadingButton } from "@mui/lab";
 
-const ProductImage = styled('img')({
-    maxWidth: '100%',
-    maxHeight: '100%',
-    objectFit: 'contain',
+const ProductImage = styled("img")({
+    maxWidth: "100%",
+    maxHeight: "100%",
+    objectFit: "contain",
 });
 
 const ProductInfo = styled(Grid)(({ theme }) => ({
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
         order: -1,
     },
 }));
@@ -28,64 +30,114 @@ const NeonTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 const BoldNeonTableCell = styled(NeonTableCell)({
-    fontWeight: 'bold',
-  });
-
+    fontWeight: "bold",
+});
 
 export default function ProductDetails() {
     const { id } = useParams<{ id: string }>();
     const [product, setProduct] = useState<Product | null>(null);
     const [loading, setLoading] = useState(true);
+    const { basket, setBasket, removeItem } = useStoreContext();
+    const [quantity, setQuantity] = useState(0);
+    const [submitting, setSubmitting] = useState(false);
+    const item = basket?.items.find((i) => i.productId === product?.id);
 
     useEffect(() => {
-        id && agent.Catalog.details(parseInt(id))
-            .then(response => setProduct(response))
-            .catch(error => console.log(error))
-            .finally(() => setLoading(false))
-    }, [id]);
+        if (item) setQuantity(item.quantity);
+        id &&
+            agent.Catalog.details(parseInt(id))
+                .then((response) => setProduct(response))
+                .catch((error) => console.log(error))
+                .finally(() => setLoading(false));
+    }, [id, item]);
 
-    if (loading) return <LoadingComponent message="Loading product..." />
+    function handleInputChange(e: any) {
+        if (e.target.value >= 0) setQuantity(parseInt(e.target.value));
+    }
 
-    if (!product) return <NotFound />
+    function handleUpdateCart() {
+        setSubmitting(true);
+        if (!item || quantity > item?.quantity) {
+            const updatedQuantity = item ? quantity - item.quantity : quantity;
+            agent.Basket.addItem(product?.id!, updatedQuantity)
+                .then((basket) => setBasket(basket))
+                .catch((error) => console.log(error))
+                .finally(() => setSubmitting(false));
+        } else {
+            const updatedQuantity = item.quantity - quantity;
+            agent.Basket.removeItem(product?.id!, updatedQuantity)
+                .then(() => removeItem(product?.id!, updatedQuantity))
+                .catch((error) => console.log(error))
+                .finally(() => setSubmitting(false));
+        }
+    }
 
+    if (loading) return <LoadingComponent message="Loading product..." />;
+
+    if (!product) return <NotFound />;
 
     return (
-        <Grid container spacing={6} alignItems='center'>
+        <Grid container spacing={6} alignItems="center">
             <ProductInfo item xs={12} md={6}>
                 <NeonTableContainer>
-                    <Table>
-                        <TableBody sx={{ fontSize: '1.1em' }}>
-                            <TableRow>
-                                <NeonTableCell colSpan={2}>
-                                    <Typography variant='h3'>{product.name}</Typography>
-                                    <Divider sx={{ mb: 2 }} />
-                                    <Typography variant='h4' color='secondary'>
-                                        €{(product.price / 100).toFixed(2)}
-                                    </Typography>
-                                </NeonTableCell>
-                            </TableRow>
-                            <TableRow>
-                                <BoldNeonTableCell>Name</BoldNeonTableCell>
-                                <NeonTableCell>{product.name}</NeonTableCell>
-                            </TableRow>
-                            <TableRow>
-                                <BoldNeonTableCell>Description</BoldNeonTableCell>
-                                <NeonTableCell>{product.description}</NeonTableCell>
-                            </TableRow>
-                            <TableRow>
-                                <BoldNeonTableCell>Category</BoldNeonTableCell>
-                                <NeonTableCell>{product.category}</NeonTableCell>
-                            </TableRow>
-                            <TableRow>
-                                <BoldNeonTableCell>Company</BoldNeonTableCell>
-                                <NeonTableCell>{product.company}</NeonTableCell>
-                            </TableRow>
-                            <TableRow>
-                                <BoldNeonTableCell>Remaining</BoldNeonTableCell>
-                                <NeonTableCell>{product.quantityRemains}</NeonTableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
+                    <Box marginX={2}>
+                        <Box marginBottom={2} />
+                        <Table>
+                            <TableBody sx={{ fontSize: "1.1em" }}>
+                                <TableRow>
+                                    <NeonTableCell colSpan={2}>
+                                        <Typography variant="h3">{product.name}</Typography>
+                                        <Divider sx={{ mb: 2 }} />
+                                        <Typography variant="h4" color="secondary">
+                                            €{(product.price / 100).toFixed(2)}
+                                        </Typography>
+                                    </NeonTableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <BoldNeonTableCell>Name</BoldNeonTableCell>
+                                    <NeonTableCell>{product.name}</NeonTableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <BoldNeonTableCell>Description</BoldNeonTableCell>
+                                    <NeonTableCell>{product.description}</NeonTableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <BoldNeonTableCell>Category</BoldNeonTableCell>
+                                    <NeonTableCell>{product.category}</NeonTableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <BoldNeonTableCell>Company</BoldNeonTableCell>
+                                    <NeonTableCell>{product.company}</NeonTableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <BoldNeonTableCell>Remaining</BoldNeonTableCell>
+                                    <NeonTableCell>{product.quantityRemains}</NeonTableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                        {/* Add spacing above TextField and LoadingButton */}
+                        <Box marginBottom={2} />
+                        <Grid container>
+                            <Grid item xs={6}>
+                                <TextField onChange={handleInputChange} variant={"outlined"} type={"number"} label={"Quantity in Cart"} fullWidth value={quantity} />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <LoadingButton
+                                    disabled={item?.quantity === quantity || (!item && quantity === 0)}
+                                    loading={submitting}
+                                    onClick={handleUpdateCart}
+                                    sx={{ height: "55px" }}
+                                    color={"warning"}
+                                    size={"large"}
+                                    variant={"contained"}
+                                    fullWidth
+                                >
+                                    {item ? "Update Quantity" : "Add to Cart"}
+                                </LoadingButton>
+                            </Grid>
+                        </Grid>
+                        <Box marginBottom={2} />
+                    </Box>
                 </NeonTableContainer>
             </ProductInfo>
             <Grid item xs={12} md={6}>
